a12c18d53ad91ad91135d884e5d64a000359706c
Victor <Victor@Victors-MacBook-Pro.local>
2019-01-16 17:40:50 +0000

    added message when ingredients array is empty to prompt user

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,12 +3,18 @@ import './Burger.css'
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
-    const transformedIngredientsArray = Object.keys(props.ingredients)
+    let transformedIngredientsArray = Object.keys(props.ingredients)
         .map(igKey => {
             return [...Array(props.ingredients[igKey])].map((_, i) => {
                 return <BurgerIngredient key={igKey + i} type={igKey}/>
             })
-        });
+        }).reduce((arr,el) => {
+            return arr.concat(el)
+        }, []);
+        if(transformedIngredientsArray.length === 0) {
+            transformedIngredientsArray = <p>FIll THIS BURGER</p>
+        }
+        console.log(transformedIngredientsArray)
     return (
         <div className='Burger'>
             <BurgerIngredient type='bread-top'/>
