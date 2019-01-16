20e4440f325e4fd579f0dace2629f4cd97530d6b
Victor <Victor@Victors-MacBook-Pro.local>
2019-01-16 17:23:15 +0000

    added functoin in burger which makes ingredients dynamic with state

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,12 +3,16 @@ import './Burger.css'
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
+    const transformedIngredientsArray = Object.keys(props.ingredients)
+        .map(igKey => {
+            return [...Array(props.ingredients[igKey])].map((_, i) => {
+                return <BurgerIngredient key={igKey + i} type={igKey}/>
+            })
+        });
     return (
         <div className='Burger'>
             <BurgerIngredient type='bread-top'/>
-            <BurgerIngredient type='cheese'/>
-            <BurgerIngredient type='bacon'/>
-            <BurgerIngredient type='meat'/>
+            {transformedIngredientsArray}
             <BurgerIngredient type='bread-bottom'/>
         </div>
     );
