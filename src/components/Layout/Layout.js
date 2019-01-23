ebfa458c22a5444c6818241bc88f735def9dd90e
Victor <Victor@Victors-MacBook-Pro.local>
2019-01-23 11:11:36 +0000

    added side draw functionality and changeed menu icon

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,19 +1,34 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Aux from '../../hoc/Aux';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import './Layout.css'
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer'
 
-const layout = (props) => {
-      return (
-        <Aux>
-        <Toolbar/>
-        <SideDrawer/>
-        <main className="LayoutContent">
-        {props.children}
-        </main>
-       </Aux>
-      ) 
-};  
+class Layout extends Component {
+      state = {
+            showSideDrawer: false
+      }
 
-export default layout;
+      sideDrawerToggleHandler = () => {
+            this.setState((prevState) => {
+                  return {showSideDrawer : !prevState.showSideDrawer};
+            })
+      }
+
+      sideDrawerClosedHandler = () => {
+            this.setState({showSideDrawer: false});
+      }
+      render () {
+            return (
+                  <Aux>
+                  <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler} />
+                  <SideDrawer open={this.state.showSideDrawer} closed={this.sideDrawerClosedHandler}/>
+                  <main className="LayoutContent">
+                        {this.props.children}
+                  </main>
+            </Aux>
+            )
+      }
+}
+
+export default Layout;
